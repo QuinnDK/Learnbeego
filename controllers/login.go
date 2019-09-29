bf57d81c15340871717193b055fac22da787b8c1
QuinnDK <18207205276@163.com>
2019-09-29 20:44:32 +0800

    第二次提交：初步了解开发流程，不太清楚get,post...

---
+++ controllers/login.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/astaxie/beego"
+
+type Logincontroller struct {
+	beego.Controller
+}
+
+//使用GET方法
+func (this *Logincontroller) Get() {
+	this.TplName = "login.html" //解析login模板
+}
+
+//使用PUT方法
+func (this *Logincontroller) Post() {
+	user := this.GetString("user")
+	pwd := this.GetString("pwd")
+	this.Ctx.WriteString("user:" + user + "password:" + pwd)
+}
--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,12 @@
 package routers
 
 import (
-	"word/controllers"
 	"github.com/astaxie/beego"
+	"word/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
-    beego.Router("hello/",&controllers.HelloControllers{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("hello/", &controllers.HelloControllers{})
+	beego.Router("/login", &controllers.Logincontroller{}, "Get:Get;Post:Post")
 }
